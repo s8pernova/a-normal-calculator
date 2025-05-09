4d0290cfd593809f1f54e50f529e11eb822d56b3
Aidan <achrunaway@gmail.com>
2025-05-09 10:55:32 -0400

    template

--- a-normal-calculator/src/App.jsx
+++ a-normal-calculator/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main-container">
+      <h1>A Normal Calculator</h1>
+      <div className="calculator-container">
+        <img src="https://m.media-amazon.com/images/I/71LgDN3WrpL._AC_UF894,1000_QL80_.jpg" className="calculator-image" />
+        Testing
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
