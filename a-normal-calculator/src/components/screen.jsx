153cd6910440d8c4e794644d308ee483bfce0bc9
achrunaway <achrunaway@gmail.com>
2025-05-13 10:10:19 -0400

    prepare for more styling

--- a-normal-calculator/src/components/screen.jsx
+++ a-normal-calculator/src/components/screen.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 const Screen = (props) => {
 	return (
-		<div className="screen">
-			<h1>{props.value}</h1>
-		</div>
+		// <div className="screen-border">
+            <div className="screen">
+                <h1>{props.value}</h1>
+            </div>
+        // </div>
 	);
 };
 
